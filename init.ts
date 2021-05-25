516e0eb524ddcebaf2429f22719b6b018f566d24
khatib <abbas.khatib@ulb.be>
2021-05-25 10:17:46 +0200

    init

---
+++ init.ts
@@ -0,0 +1,32 @@
+let fs = require('fs');
+const arguments = process.argv.slice(2)
+if(arguments.length ===6){
+    // if if --generator and project type next and --resource (create specific resource --resource)
+    if(arguments[2] ==='--generator' && arguments[3] ==='next' && arguments[4] ==='--resource'){
+        // directory to check if exists
+        const dir = './src';
+        // check if directory exists
+        if (fs.existsSync(dir)) {
+            console.log('Directory exists!');
+        } else {
+            fs.mkdirSync(dir);
+            console.log('Directory not found.');
+        }
+
+    }
+}else{
+    console.log('\x1b[31m','sdfsdf')
+}
+/*const getEntryPoints =  ()=>{
+  return axios.get('').then(
+      (response)=>{
+          console.log('response')
+          console.log(response)
+      }
+/*
+fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+});*/
+
+
